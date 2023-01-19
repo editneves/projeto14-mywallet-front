4f64c1d456ba9fe63f7eced48fed4b3c7ec1776d
root <root@DESKTOP-0N365CC>
2023-01-19 15:40:17 -0300

    retirei os dados do programa/

--- src/App.js
+++ src/App.js
@@ -1,55 +1,22 @@
 import styled from 'styled-components'
-// import RoutersComponents from './RoutersComponents'
-import { AuthContext } from '../src/components/AuthContext'
-import { useState } from 'react'
 
 export default function App() {
-
-  const [form, setForm] = useState({ email: '', password: '' })
-  const [cadastro, setCadastro] = useState({
-    email: '',
-    name: '',
-    cpf: '',
-    password: '',
-  })
-  
-  const [user, setUser] = useState(null)
-  const [usuario, setUsuario] = useState({
-    name: '',
-    cpf: '',
-    email: '',
-    currentPassword: '',
-    newPassword: '',
-  })
-
-  return (
-    <AuthContext.Provider
-      value={{
-        form,
-        setForm,
-        user,
-        setUser,
-        cadastro,
-        setCadastro,
-        usuario, 
-        setUsuario,
-      }}
-    >
-      <Container>
-        {/* <RoutersComponents /> */}
-      </Container>
-    </AuthContext.Provider>
-  )
+return (
+  <Container>
+    <div>
+      lalala
+    </div>
+  </Container>
+)
 }
-
-const Container = styled.div`
-  background-color: #9567BE;
-  width: 100vw;
-  min-height: 100vh;
+  const Container = styled.div`
+  width: 375px;
+  height: 667px;
+  background: #0e0e13;
   display: flex;
   flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: center;
   align-items: center;
-  margin: 0px;
-  padding: 0px;
-  padding-bottom: 200px;
+  align-content: space-between;
 `
